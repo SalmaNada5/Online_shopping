38379c5f18d4125bfdcf164fa529e1ea472f26a7
SalmaNada5 <salma.nada.fci@gmail.com>
2022-12-28 23:41:14 +0200

    added clothes.js, bags.js, perfumes.js

---
+++ clothes/clothes.js
@@ -0,0 +1,7 @@
+var count=0
+function myFunction() {
+    document.getElementById("btn").style.color = "red";
+    count+=1;
+    console.log(count);
+    alert('The payment has been successfully completed, and what you requested will be delivered to you ->'+'required number: '+count);  
+}
