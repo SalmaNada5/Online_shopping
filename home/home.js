27531ee214ebce3ef50ae934a0d14557e89c7ef4
Rwan Adel <57010052+rwanadel@users.noreply.github.com>
2022-12-29 02:31:25 +0200

    js

--- home/home.js
+++ home/home.js
@@ -1,10 +1,10 @@
 //select cover
-let picCover=document.querySelector(".img1");
+let picCover=document.querySelector(".cover");
 
 
 //array of image
-let imgArray=["1.jpg","2,jpg","3.jpg","4.jpg","5.jpg","6.jpg"];
+let imgArray=["cover.jpg","1.jpg","2.jpg","3.jpg","4.jpg","6.jpg"];
 setInterval(()=>{
     let randomNumber=Math.floor(Math.random() * imgArray.length);
     picCover.style.backgroundImage='url("imgs/'+ imgArray[randomNumber] +' ")';
-},1000)
+},3000)
